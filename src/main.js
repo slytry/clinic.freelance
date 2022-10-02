22f10c85faf7e6173bab4d439a12174384f1693c
Artur Procenko <artur.procenko@chulakov.ru>
2022-10-02 15:59:53 +0300

    feat: add slider

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import '/style/main.scss';
+
+const swiper = new Swiper('.mySwiper', {
+    autoplay: {
+        delay: 5000,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    allowTouchMove: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    mousewheel: true,
+    keyboard: true,
+});
