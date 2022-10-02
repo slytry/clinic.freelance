3ae3dbc6e1eb77da6a978e462a61f933548a845e
Nikita Naumov <naumov99.rst@gmail.com>
2022-10-02 22:01:48 +0300

    WIP: home progress

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import '/style/main.scss';
 import './home.scss';
 
+const button = document.querySelector('.header__button');
+const menu = document.querySelector('.menu');
+const buttonRows = document.querySelectorAll('.header__button-row');
+
+export const greeting = () => {
+    document.addEventListener('click', (e) => {
+        if (!menu.contains(e.target) && !button.contains(e.target)) {
+            menu.classList.remove('menu--open');
+        }
+    });
+
+    button.addEventListener('click', () => {
+        menu.classList.toggle('menu--open');
+        buttonRows.forEach((i) => {
+            i.classList.toggle('header__button-row--open');
+        });
+    });
+};
+
 const swiper = new Swiper('.mySwiper', {
     autoplay: {
         delay: 5000,
