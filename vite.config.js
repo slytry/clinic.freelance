580dd97580c08c6c7fe5814b79f640379930832f
Nikita Naumov <naumov99.rst@gmail.com>
2022-10-02 19:33:13 +0300

    feat: add html plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import { resolve } from 'path';
+
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
     root: 'src',
-    // base: '/public/',
     build: {
         rollupOptions: {
             input: {
@@ -22,4 +23,38 @@ export default defineConfig({
     //         },
     //     },
     // },
+    plugins: [
+        createHtmlPlugin({
+            minify: true,
+            /**
+             * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+             * @default src/main.ts
+             */
+            entry: 'src/main.ts',
+            /**
+             * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+             * @default index.html
+             */
+            template: 'public/index.html',
+
+            /**
+             * Data that needs to be injected into the index.html ejs template
+             */
+            inject: {
+                data: {
+                    title: 'index',
+                    injectScript: `<script src="./inject.js"></script>`,
+                },
+                tags: [
+                    {
+                        injectTo: 'body-prepend',
+                        tag: 'div',
+                        attrs: {
+                            id: 'tag',
+                        },
+                    },
+                ],
+            },
+        }),
+    ],
 });
