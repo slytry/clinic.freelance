a869a77280dd9d585b9739dcfc94e8e7c2477a7a
Nikita Naumov <naumov99.rst@gmail.com>
2022-10-03 19:40:26 +0300

    chore: clear

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-// import { createHtmlPlugin } from 'vite-plugin-html';
-
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
@@ -19,38 +17,4 @@ export default defineConfig({
             },
         },
     },
-    // css: {
-    //     preprocessorOptions: {
-    //         scss: {
-    //             additionalData: `@use '/style/absract' as *;`,
-    //         },
-    //     },
-    // },
-    // plugins: [
-    //     createHtmlPlugin({
-    //         minify: false,
-    //         pages: [
-    //             {
-    //                 entry: 'main.ts',
-    //                 filename: 'index.html',
-    //                 template: 'index.html',
-    //                 injectOptions: {
-    //                     data: {
-    //                         title: 'Семья',
-    //                     },
-    //                 },
-    //             },
-    //             {
-    //                 entry: 'src/pages/services.js',
-    //                 filename: 'services.html',
-    //                 template: 'src/pages/services.html',
-    //                 injectOptions: {
-    //                     data: {
-    //                         title: 'Services',
-    //                     },
-    //                 },
-    //             },
-    //         ],
-    //     }),
-    // ],
 });
