49d0ed29e6360210e5577948e64ebf12b09653f0
Nikita Naumov <naumov@dev.com>
2022-10-24 00:09:19 +0300

    WIP: try to set EJS

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import '/style/main.scss';
-import './home.scss';
-import './main-intro.scss';
-import './scripts/burger-menu';
-import './scripts/swiper';
--- vite.config.js
+++ vite.config.js
@@ -1,63 +1,75 @@
-import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import viteImagemin from 'vite-plugin-imagemin';
 import zipPack from 'vite-plugin-zip-pack';
-import vitePugPlugin from 'vite-plugin-pug-transformer';
-
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { path, root, outDir } from './viteConfig/config';
 
 export default defineConfig({
 	root,
 	resolve: {
 		alias: {
-			'@': root,
+			'@pages/*': ['pages/*'],
+			'@style/*': ['style/*'],
+			'@images/*': ['images/*'],
 		},
 	},
 	build: {
 		outDir,
 		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				main: resolve(root, 'index.html'),
-				analisys: resolve(root, 'pages', 'analisys', 'index.html'),
-				dent: resolve(root, 'pages', 'dent', 'index.html'),
-				med: resolve(root, 'pages', 'med', 'index.html'),
-				services: resolve(root, 'pages', 'services', 'index.html'),
-				service: resolve(root, 'pages', 'service', 'index.html'),
-				reception: resolve(root, 'pages', 'reception', 'index.html'),
-			},
-		},
+		// rollupOptions: {
+		// 	input: {
+		// 		main: resolve(root, 'index.html'),
+		// 		analisys: resolve(root, 'pages', 'analisys', 'index.html'),
+		// 		dent: resolve(root, 'pages', 'dent', 'index.html'),
+		// 		med: resolve(root, 'pages', 'med', 'index.html'),
+		// 		services: resolve(root, 'pages', 'services', 'index.html'),
+		// 		service: resolve(root, 'pages', 'service', 'index.html'),
+		// 		reception: resolve(root, 'pages', 'reception', 'index.html'),
+		// 	},
+		// },
 	},
 	plugins: [
 		zipPack(),
-		vitePugPlugin(),
-		viteImagemin({
-			gifsicle: {
-				optimizationLevel: 7,
-				interlaced: false,
-			},
-			optipng: {
-				optimizationLevel: 7,
-			},
-			mozjpeg: {
-				quality: 20,
-			},
-			pngquant: {
-				quality: [0.8, 0.9],
-				speed: 4,
-			},
-			svgo: {
-				plugins: [
-					{
-						name: 'removeViewBox',
-					},
-					{
-						name: 'removeEmptyAttrs',
-						active: false,
-					},
-				],
-			},
+		createHtmlPlugin({
+			minify: false,
+			pages: [
+				{
+					entry: resolve(path.home, 'index.js'),
+					filename: 'index.html',
+				},
+				{
+					entry: resolve(path.anal, 'index.js'),
+					filename: 'index.html',
+				},
+			],
 		}),
+		// viteImagemin({
+		// 	gifsicle: {
+		// 		optimizationLevel: 7,
+		// 		interlaced: false,
+		// 	},
+		// 	optipng: {
+		// 		optimizationLevel: 7,
+		// 	},
+		// 	mozjpeg: {
+		// 		quality: 20,
+		// 	},
+		// 	pngquant: {
+		// 		quality: [0.8, 0.9],
+		// 		speed: 4,
+		// 	},
+		// 	svgo: {
+		// 		plugins: [
+		// 			{
+		// 				name: 'removeViewBox',
+		// 			},
+		// 			{
+		// 				name: 'removeEmptyAttrs',
+		// 				active: false,
+		// 			},
+		// 		],
+		// 	},
+		// }),
 	],
 });
