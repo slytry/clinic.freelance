038f98d67d37bbeec87441704b84d58116f044ac
Nikita Naumov <naumov99.rst@gmail.com>
2022-11-12 21:44:47 +0300

    пиздец

--- src/pages/analisys/index.js
+++ src/pages/analisys/index.js
@@ -2,32 +2,4 @@
 import '/style/main.scss';
 import './analisys.scss';
 import '/home.scss';
-import '/scripts/burger-menu';
-
-export const swiper = new Swiper('.mySwiper', {
-	autoplay: {
-		delay: 5000,
-	},
-
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	allowTouchMove: true,
-
-	pagination: {
-		el: '.swiper-pagination',
-	},
-	keyboard: true,
-});
-
-export const swiper2 = new Swiper('.mySwiper2', {
-	slidesPerView: 4,
-	centeredSlides: true,
-	spaceBetween: 30,
-	grabCursor: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-});
+import '/scripts/main';
